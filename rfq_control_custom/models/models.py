d5928ba443362c0772d3baee2015a496a6102541
Amjdhassan <Amjd_hassan@yahoo.com>
2023-01-17 01:01:08 -0800

    budget control

--- rfq_control_custom/models/models.py
+++ rfq_control_custom/models/models.py
@@ -1,16 +1,61 @@
-from odoo import models, fields, api
+from odoo import models, fields, api , _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import datetime
+import datetime
+from datetime import datetime
 
 
-class rfq_control_custom(models.Model):
-    _inherit ="crossovered.budget.lines"
-    _description = 'rfq_control_custom.rfq_control_custom'
+class RfqControlCustom(models.Model):
+    _inherit = "crossovered.budget.lines"
+
 
     department_id = fields.Many2one("hr.department",string="department")
-    # value = fields.Integer()
-    # value2 = fields.Float(compute="_value_pc", store=True)
-    # description = fields.Text()
 
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     for record in self:
-    #         record.value2 = float(record.value) / 100
+class ApprovalReq(models.Model):
+    _inherit = 'approval.request'
+
+    def action_confirm(self):
+        d_date = datetime.today()
+        for rec in self:
+            if rec.product_line_ids:
+                for line in rec.product_line_ids:
+                    if not line.product_id.property_account_expense_id:
+                        raise ValidationError(
+                            _("No expense account available for One Product in list!")
+                        )
+                    else:
+                        position_ids = self.env["account.budget.post"].search(
+                            [
+                                ("account_ids", "in", line.product_id.property_account_expense_id.id),
+                                ("company_id", "=", self.env.user.company_id.id),
+                            ]
+
+                        )
+                        for post in position_ids:
+                            budget_lines = self.env["crossovered.budget.lines"].search(
+                                [
+                                    ("general_budget_id", "=", post.id),
+                                    ("department_id", "=", self.env.user.department_id.id),
+                                ],limit=1
+
+                            )
+                            print(budget_lines.crossovered_budget_id.state)
+                            if budget_lines:
+                                if budget_lines.crossovered_budget_id.state == 'done' and d_date.date() >= budget_lines.crossovered_budget_id.date_from and d_date.date() <= budget_lines.crossovered_budget_id.date_to  :
+                                    if not line.product_id.standard_price or line.product_id.standard_price == 0 :
+                                        raise ValidationError(
+                                            _("No product cost for one item in list !:" + line.product_id.name )
+                                        )
+                                    elif (line.product_id.standard_price * line.quantity) + abs(budget_lines.theoritical_amount) > abs(budget_lines.planned_amount):
+                                            raise ValidationError(
+                                                _("The planned budget will be exceeded !:")
+                                            )
+
+                            else:
+                                raise ValidationError(
+                                    _("No budget available !")
+                                )
+
+
+        super(ApprovalReq, self).action_confirm()
+        pass
