85cb9518d773ebc93501957511d31b988d70818c
Amjdhassan <Amjd_hassan@yahoo.com>
2023-01-20 13:36:38 -0800

    branchs

--- rfq_control_custom/controllers/controllers.py
+++ rfq_control_custom/controllers/controllers.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+import logging
+from odoo import http
+_logger = logging.getLogger(__name__)
 
 
-# class RfqControlCustom(http.Controller):
-#     @http.route('/rfq_control_custom/rfq_control_custom', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class RfqControlCstm(http.Controller):
 
-#     @http.route('/rfq_control_custom/rfq_control_custom/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('rfq_control_custom.listing', {
-#             'root': '/rfq_control_custom/rfq_control_custom',
-#             'objects': http.request.env['rfq_control_custom.rfq_control_custom'].search([]),
-#         })
+    # @http.route('/xlsx_reports', type='http', auth='user', methods=['POST'], csrf=False)
+    @http.route(['/rfq_control_custom'], type='http', auth='public', csrf=False)
+    def index(self, **kw):
+
+        _logger.info('\n\n\n\nHELLO, WORLD')
+        return "Hello, world"
+
+    @http.route('/rfq_control_custom/rfq_control_custom/objects', auth='none')
+    def list(self, **kw):
+        return http.request.render('rfq_control_custom.listing', {
+            'root': '/rfq_control_custom/rfq_control_custom',
+            'objects': http.request.env['rfq_control_custom.rfq_control_custom'].search([]),
+        })
 
 #     @http.route('/rfq_control_custom/rfq_control_custom/objects/<model("rfq_control_custom.rfq_control_custom"):obj>', auth='public')
 #     def object(self, obj, **kw):
