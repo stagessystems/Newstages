edfb7789e7b247e733a5716eb7f54e52751c57de
Amjdhassan <Amjd_hassan@yahoo.com>
2023-01-28 22:43:15 -0800

    custom layout

---
+++ external_layout/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "External Layout",
+    'summary': """""",
+    'description': """
+        External Layout
+    """,
+    'author': "AmjedHassan",
+    'website': "https://www.linkedin.com/in/amjed_hassan",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'data/report_layout.xml',
+        'views/external_layout_custom_template_report.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
